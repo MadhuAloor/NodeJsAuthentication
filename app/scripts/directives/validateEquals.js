4f72859997d2e2d0d20cc744940dd1a0ab6a73d3
MadhuAloor <madhu.aloor@gmail.com>
2016-11-10 00:37:11 +0530

    Add files via upload

--- app/scripts/directives/validateEquals.js
+++ app/scripts/directives/validateEquals.js
@@ -1,1 +1,27 @@
+'use strict';
 
+/**
+ * @ngdoc directive
+ * @name authenticationPsApp.directive:sameAs
+ * @description
+ * # sameAs
+ */
+angular.module('authenticationPsApp')
+  .directive('validateEquals', function () {
+    return {
+      require:'ngModel',
+      link: function postLink(scope, element, attrs,ngModelCtrl) {
+        function validateEqual(value){
+          var valid = (value === scope.$eval(attrs.validateEquals));
+          ngModelCtrl.$setValidity('equal',valid);
+          return valid? value : undefined;
+        }
+        ngModelCtrl.$parsers.push(validateEqual);
+        ngModelCtrl.$formatters.push(validateEqual);
+
+        scope.$watch(attrs.validateEquals,function(){
+          ngModelCtrl.$setViewValue(ngModelCtrl.$viewValue);
+        })
+      }
+    };
+  });
