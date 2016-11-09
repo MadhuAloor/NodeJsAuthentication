70da976b6b4001fbc4d3b20353cf25c902e1bb68
MadhuAloor <madhu.aloor@gmail.com>
2016-11-10 00:31:02 +0530

    Create app.js

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name authenticationPsApp
+ * @description
+ * # authenticationPsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('authenticationPsApp', ['ui.router']);
