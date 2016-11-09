23c3e0e63e5fde52cbafe1f8eb32f475fecde3ab
MadhuAloor <madhu.aloor@gmail.com>
2016-11-10 00:46:58 +0530

    Add files via upload

--- api/services/jwt.js
+++ api/services/jwt.js
@@ -1,1 +1,25 @@
+/**
+ * Created by z013n8g on 11/9/16.
+ */
 
+var crypto = require('crypto');
+
+exports.encode = function(payload,secret){
+  algorithm = 'HS256';
+
+  var header = {
+    typ:'JWT',
+    alg:algorithm
+  };
+
+  var jwt  = base64Encode(JSON.stringify(header)) + '.' + base64Encode(JSON.stringify(payload))
+
+   return jwt += '.'+sign(jwt,secret);
+}
+function sign(str,key){
+     return crypto.createHmac('sha256',key).update(str).digest('base64');
+}
+
+function base64Encode(str){
+  return new Buffer(str).toString('base64')
+}
