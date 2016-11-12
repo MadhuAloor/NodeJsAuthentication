586b3066f2b2bcf707aa81c5df334ce7f3024caf
MadhuAloor <madhu.aloor@gmail.com>
2016-11-13 02:23:03 +0530

    used node simple jwt module

--- api/api.js
+++ api/api.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 var mongoose = require('mongoose');
 var app = express();
 var User = require('./model/User.js');
-var jwt = require('./services/jwt.js');
+var jwt = require('jwt-simple');
 
 app.use(bodyParser.json());
 
@@ -15,23 +15,49 @@ app.use(cors());
 
 app.post('/register',function(req,res){
   var user = req.body;
-  var newUser = new User.model({
+  var newUser = new User({
     email: user.email,
     password: user.password
   })
 
-  var payload ={
-    iss: req.hostname,
-    sub:newUser.id
-  }
-
-  var token = jwt.encode(payload,"shhh....");
-
   newUser.save(function(err){
-    res.status(200).send({user:newUser.toJSON(),token:token});
+     createSendToken(newUser,res);
   })
 })
 
+app.post('/login',function(req,res){
+  req.user = req.body;
+
+  var searchUser = {
+    email: req.user.email
+  };
+
+  User.findOne(searchUser,function(err,user){
+
+    if(err)throw err;
+
+    if(!user)
+      return res.status(401).send({message:"you are not authorized"});
+    user.comparePasswords(req.user.password,function(err,isMatch){
+      if(err)throw err;
+
+      if(!isMatch)
+        return res.status(401).send({message:"you are not authorized"});
+      createSendToken(user,res);
+    });
+  })
+})
+
+function createSendToken(user,res){
+  var payload ={
+    sub:user.id
+  }
+
+
+  var token = jwt.encode(payload,"shhh....");
+  res.status(200).send({user:user.toJSON(),token:token});
+
+}
 var jobs = [
    'Cook',
    'Special Cook',
